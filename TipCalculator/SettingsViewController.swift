6086ffccd502394ef4d7449a1699ca9d255d60c7
Heekyung Kim <Heekyungkim.tech@gmail.com>
2019-01-03 17:14:45 -0500

    initial commit

---
+++ TipCalculator/SettingsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SettingsViewController.swift
+//  TipCalculator
+//
+//  Created by Heekyung kim on 1/3/19.
+//  Copyright © 2019 Heekyung kim. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Sets the title in the Navigation Bar
+        self.title = "Tip Calculator"
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
